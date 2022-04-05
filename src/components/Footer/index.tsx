4df664544f4d880a916b32b6fb85bf0de9136c0d
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2022-04-05 14:01:55 -0300

    firist commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2023 por Divinas. Orgulhosamente criado com Wix.com</p>
+      <address>
+        Divinas Ltda. - CPF/CNPJ: 12.345.678/0000-01 - Av. Bernardino de Campos,
+        98 - São Paulo, SP 12345-678
+      </address>
+      <p>
+        <a href="mailto:teste@gmail.com">info@meusite.com</a>
+        <a href="tel:113456-7890"> Tel: (11) 3456-7890</a>
+      </p>
+    </footer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,14 @@
+import { Footer } from '../../components/Footer'
 import { Navbar } from '../../components/Navbar'
 
+import './Home.css'
+
 export function Home() {
-  return <Navbar />
+  return (
+    <>
+      <Navbar />
+      <main className="home"></main>
+      <Footer />
+    </>
+  )
 }
