1baa4b0dd4d4644a87255af3bb3f2dbde705f350
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2022-04-12 20:57:25 -0300

    finalizando rodapé de recurso

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,29 @@
+import { FooterBenefits } from '../FooterBenefits'
+import { FooterNavigation } from '../FooterNavegation'
+import { FooterNewsletter } from '../FooterNewsletter'
+import { FooterSocialMedia } from '../FooterSocialMedia'
 import './Footer.css'
 
 export function Footer() {
   return (
-    <footer className="footer">
-      <p>© 2023 por Divinas. Orgulhosamente criado com Wix.com</p>
-      <address>
-        Divinas Ltda. - CPF/CNPJ: 12.345.678/0000-01 - Av. Bernardino de Campos,
-        98 - São Paulo, SP 12345-678
-      </address>
-      <p>
-        <a href="mailto:teste@gmail.com">info@meusite.com</a>
-        <a href="tel:113456-7890"> Tel: (11) 3456-7890</a>
-      </p>
-    </footer>
+    <div className="footer">
+      <div className="container">
+        <FooterNewsletter />
+        <FooterNavigation />
+        <FooterBenefits />
+        <FooterSocialMedia />
+      </div>
+      <footer>
+        <p>© 2023 por Divinas. Orgulhosamente criado com Wix.com</p>
+        <address>
+          Divinas Ltda. - CPF/CNPJ: 12.345.678/0000-01 - Av. Bernardino de
+          Campos, 98 - São Paulo, SP 12345-678
+        </address>
+        <p>
+          <a href="mailto:teste@gmail.com">info@meusite.com</a>
+          <a href="tel:113456-7890"> Tel: (11) 3456-7890</a>
+        </p>
+      </footer>
+    </div>
   )
 }
---
+++ src/components/FooterNavegation/index.tsx
@@ -0,0 +1,37 @@
+import './FooterNavigation.css'
+
+export function FooterNavigation() {
+  return (
+    <nav className="navegation">
+      <ul>
+        <li>
+          <a href="/">Comprar Tudo</a>
+        </li>
+        <li>
+          <a href="/">Apliques de Cabelo</a>
+        </li>
+        <li>
+          <a href="/">Cílios</a>
+        </li>
+        <li>
+          <a href="/">Acessórios</a>
+        </li>
+        <li>
+          <a href="/">Sobre Nós</a>
+        </li>
+        <li>
+          <a href="/">Contato</a>
+        </li>
+        <li>
+          <a href="/">Envio e Retorno</a>
+        </li>
+        <li>
+          <a href="/">Polítca da Loja</a>
+        </li>
+        <li>
+          <a href="/">Perguntas Frequentes</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
