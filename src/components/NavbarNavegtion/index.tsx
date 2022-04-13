f29c8e9ae0f17b96584c3baa64e50b2dbf863a3b
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2022-04-13 20:20:39 -0300

    firist commit

--- src/components/NavbarNavegtion/index.tsx
+++ src/components/NavbarNavegtion/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './NavbarNavegation.css'
 
 export function NavbarNavegtion({ active }: { active: boolean }) {
@@ -8,8 +9,12 @@ export function NavbarNavegtion({ active }: { active: boolean }) {
         <li>Apliques de cabelo</li>
         <li>Cílios</li>
         <li>Acessórios</li>
-        <li>Sobre</li>
-        <li>Contato</li>
+        <li>
+          <Link to="/about">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contato</Link>
+        </li>
       </ul>
     </nav>
   )
